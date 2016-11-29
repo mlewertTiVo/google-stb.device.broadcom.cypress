b706b23c2eee4748ef70aed0ee6708336a4e26cf
Pierre Couillaud <Pierre.Couillaud@broadcom.com>
2016-11-29 07:20:55 -0800

    cypress: platform configuration (matching dboards).
    - enable a|b update mode support.
    - enable verity support (dev key signing).
    
    refs #SWANDROID-2980
    
    Change-Id: I25aab00dbcb567d354b6cd1f1ad6f0f055d72c46
    Signed-off-by: Pierre Couillaud <Pierre.Couillaud@broadcom.com>

--- cypress.mk
+++ cypress.mk
@@ -10,7 +10,7 @@ export ANDROID_PRODUCT_OUT       := cypress
 LOCAL_DEVICE_RCS                 := device/broadcom/common/rcs/init.rc:root/init.cypress.rc
 LOCAL_DEVICE_RCS                 += device/broadcom/common/rcs/init.nx.rc:root/init.nx.rc
 LOCAL_DEVICE_RCS                 += device/broadcom/common/rcs/ueventd.rc:root/ueventd.cypress.rc
-LOCAL_DEVICE_RCS                 += device/broadcom/common/rcs/init.fs.default.rc:root/init.fs.rc  # NOT verity
+LOCAL_DEVICE_RCS                 += device/broadcom/common/rcs/init.fs.verity.rc:root/init.fs.rc  # verity
 LOCAL_DEVICE_RCS                 += device/broadcom/common/rcs/init.eth.eth0.rc:root/init.eth.rc   # uses 'eth0'
 LOCAL_DEVICE_RCS                 += device/broadcom/cypress/rcs/init.block.rc:root/init.block.rc   # block devices
 LOCAL_DEVICE_RCS                 += device/broadcom/cypress/rcs/init.bcm.usb.rc:root/init.bcm.usb.rc   # uses 'configfs'
@@ -21,8 +21,8 @@ LOCAL_DEVICE_RECOVERY_RCS        += device/broadcom/cypress/rcs/init.block.rc:ro
 LOCAL_DEVICE_RECOVERY_RCS        += device/broadcom/cypress/rcs/init.recovery.usb.rc:root/init.recovery.usb.rc   # uses 'configfs'
 export LOCAL_DEVICE_RECOVERY_RCS
 
-LOCAL_DEVICE_FSTAB               := device/broadcom/common/fstab/fstab.squashfs:root/fstab.bcm
-LOCAL_DEVICE_FSTAB               += device/broadcom/common/fstab/fstab.squashfs:root/fstab.cypress
+LOCAL_DEVICE_FSTAB               := device/broadcom/common/fstab/fstab.verity.squashfs.ab-update:root/fstab.bcm
+LOCAL_DEVICE_FSTAB               += device/broadcom/common/fstab/fstab.verity.squashfs.ab-update:root/fstab.cypress
 export LOCAL_DEVICE_FSTAB
 
 LOCAL_DEVICE_RECOVERY_FSTAB      := device/broadcom/common/recovery/fstab.squashfs/recovery.fstab
@@ -55,13 +55,15 @@ export LOCAL_DEVICE_AON_GPIO     := device/broadcom/cypress/aon_gpio.cfg:system/
 export LOCAL_DEVICE_KEY_POLL     := device/broadcom/common/keylayout/gpio_keys_polled.kl:system/usr/keylayout/gpio_keys_polled_5.kl
 export LOCAL_DEVICE_BT_CONFIG    := device/broadcom/cypress/bluetooth/vnd_cypress.txt
 export LOCAL_DEVICE_USERDATA     := 5994691584 # ~5.58GB
-export LOCAL_DEVICE_GPT          := device/broadcom/common/gpts/default_v2.conf
+export LOCAL_DEVICE_GPT          := device/broadcom/common/gpts/ab-u.conf
 export HW_ENCODER_SUPPORT        := n
 export HW_WIFI_NIC_SUPPORT       := y
 export BT_RFKILL_SUPPORT         := y
 export LOCAL_SYSTEMIMAGE_SQUASHFS := y
 export ANDROID_ENABLE_BT         := uart
 export V3D_VARIANT               := vc5
+export HW_AB_UPDATE_SUPPORT      := y
+export LOCAL_DEVICE_USE_VERITY   := y
 export LOCAL_DEVICE_REFERENCE_BUILD := device/broadcom/cypress/reference_build.mk
 
 # reduce nexus.ko to make room for wifi-nic.
@@ -76,6 +78,7 @@ LOCAL_DEVICE_KERNEL_CMDLINE      := mem=2048m@0m
 LOCAL_DEVICE_KERNEL_CMDLINE      += bmem=512m@1528m
 LOCAL_DEVICE_KERNEL_CMDLINE      += brcm_cma=760m@768m
 LOCAL_DEVICE_KERNEL_CMDLINE      += ramoops.mem_address=0x7F800000 ramoops.mem_size=0x800000 ramoops.console_size=0x400000 pmem=8m@2040m
+LOCAL_DEVICE_KERNEL_CMDLINE      += rootwait init=/init ro
 export LOCAL_DEVICE_KERNEL_CMDLINE
 
 # baseline the common support.
