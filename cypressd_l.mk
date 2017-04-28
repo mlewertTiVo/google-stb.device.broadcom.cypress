5ebd6e11d85644c21a3ce6044608c7219d87078a
Pierre Couillaud <pierre.couillaud@broadcom.com>
2017-04-28 06:56:11 -0700

    cypress: restore cypressd and add cypressd_l (for legacy boot).
    refs #SWANDROID-3988
    
    Change-Id: Id5893cd77415d507f45cf9dc0459eb6a0f48155e
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- cypressd_l.mk
+++ cypressd_l.mk
@@ -11,33 +11,29 @@ LOCAL_DEVICE_RCS                 := device/broadcom/common/rcs/init.rc:root/init
 LOCAL_DEVICE_RCS                 += device/broadcom/cypress/rcs/ueventd.rc:root/ueventd.cypressd.rc
 LOCAL_DEVICE_RECOVERY_RCS        := device/broadcom/common/rcs/init.recovery.rc:root/init.recovery.cypressd.rc
 
-LOCAL_DEVICE_FSTAB               := device/broadcom/common/fstab/fstab.verity.squashfs.ab-update.v2:root/fstab.cypressd
-LOCAL_DEVICE_FSTAB               += device/broadcom/common/fstab/fstab.verity.squashfs.ab-update.v2:root/fstab.bcm
+LOCAL_DEVICE_FSTAB               := device/broadcom/common/fstab/fstab.default.v2:root/fstab.cypressd
+LOCAL_DEVICE_FSTAB               += device/broadcom/common/fstab/fstab.default.v2:root/fstab.bcm
 export LOCAL_DEVICE_FSTAB
 
-LOCAL_DEVICE_RECOVERY_FSTAB      := device/broadcom/common/recovery/fstab.ab-update/recovery.fstab
+LOCAL_DEVICE_RECOVERY_FSTAB      := device/broadcom/common/recovery/fstab.default/recovery.fstab
 export LOCAL_DEVICE_RECOVERY_FSTAB
 
 # compile the media codecs for the device.
 LOCAL_DEVICE_MEDIA               := device/broadcom/common/media/media_codecs_no_legacy_enc.xml:system/etc/media_codecs.xml
 LOCAL_DEVICE_MEDIA               += device/broadcom/common/media/media_codecs_performance_no_legacy_enc.xml:system/etc/media_codecs_performance.xml
 
-export LOCAL_SYSTEMIMAGE_SQUASHFS := y
-export HW_AB_UPDATE_SUPPORT      := y
-
-export LOCAL_DEVICE_SEPOLICY_BLOCK := device/broadcom/cypress/sepolicy-block-v2
-export LOCAL_DEVICE_GPT          := device/broadcom/common/gpts/ab-u.conf
-export LOCAL_DEVICE_USE_VERITY   := y
+export LOCAL_DEVICE_SEPOLICY_BLOCK := device/broadcom/cypress/sepolicy-block-v3
+export LOCAL_DEVICE_GPT          := device/broadcom/common/gpts/default_v2.conf
 
 # common to all cypress devices.
 include device/broadcom/cypress/common.mk
 
 # kernel command line.
-LOCAL_DEVICE_KERNEL_CMDLINE      += bmem=682m@416m
-LOCAL_DEVICE_KERNEL_CMDLINE      += brcm_cma=500m@1104m
+LOCAL_DEVICE_KERNEL_CMDLINE      += bmem=698m@416m
+LOCAL_DEVICE_KERNEL_CMDLINE      += brcm_cma=500m@1120m
 
-# no legacy decoder (vp9, h263, mpeg4) in hardware s.2
-export HW_DECODER_LEGACY_SUPPORT := n
+# no legacy decoder (vp8, h263, mpeg4) in hardware s.2
+export HW_HVD_REVISION := S
 # v3d mmu available.
 export HW_GPU_MMU_SUPPORT        := y
 # dolby MS12 (use default variant by setting 'y').
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,4 +4,5 @@ add_lunch_combo cypress-user
 add_lunch_combo aosp_cypress-userdebug
 
 add_lunch_combo cypressd-userdebug
+add_lunch_combo cypressd_l-userdebug
 
