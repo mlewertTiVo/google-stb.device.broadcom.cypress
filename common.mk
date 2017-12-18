ce8830116724869faddc3aadc6c0a12d14a5bf56
Pierre Couillaud <pierre.couillaud@broadcom.com>
2017-12-18 06:56:06 -0800

    cypress: enable binary distribution build.
    Bug:65028043
    Change-Id: Ic10bc4add3911c1f97c14531a3681afeaed43134
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- common.mk
+++ common.mk
@@ -8,6 +8,7 @@ export PLATFORM                  := 97271
 # binary distribution
 export BCM_BINDIST_LIBS_ROOT     := vendor/broadcom/prebuilts/nximg/4.1/cypress
 export BCM_BINDIST_KNL_ROOT      := device/broadcom/cypress-kernel/4.1
+export BCM_DIST_FORCED_BINDIST   := y
 
 # compile the rc's for the device.
 ifeq ($(LOCAL_DEVICE_FULL_TREBLE),y)
