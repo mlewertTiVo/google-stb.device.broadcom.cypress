cd3329bb817eb1b9af5bcb37b2cf1f11b7e68d74
Jerry Wong <jerry.wong@broadcom.com>
2018-07-04 15:34:01 -0700

    cypress: allow mixed environment zd|zb support with pak
    refs #SWRAA-304
    
    Change-Id: I4ed78a0da037148a3b891bcfd5604c0aeea4fd0e
    (cherry picked from commit c3d6ac35d6df3d0fc11c292d7d2048983daafb32)

--- common.mk
+++ common.mk
@@ -45,7 +45,9 @@ export LOCAL_DEVICE_REFERENCE_BUILD := device/broadcom/cypress/reference_build.m
 export BOLT_BOARD_VB             := BCM97271T
 export LOCAL_DEVICE_SYSTEM_VERITY_PARTITION := /dev/block/platform/rdb/f0200200.sdhci/by-name/system
 export LOCAL_DEVICE_VENDOR_VERITY_PARTITION := /dev/block/platform/rdb/f0200200.sdhci/by-name/vendor
-export LOCAL_DEVICE_PAK_BINARY   := pak.7271.zd.bin
+
+export LOCAL_DEVICE_PAK_BINARY     := pak.7271.zb.bin
+export LOCAL_DEVICE_PAK_BINARY_ALT := pak.7271.zd.bin
 
 # dtu enabled.
 export HW_DTU_SUPPORT            := y
