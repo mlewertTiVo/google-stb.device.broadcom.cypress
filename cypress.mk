94aba5552b4171d1c823cfd0fc0cd8b8cfea53bb
Pierre Couillaud <pierre.couillaud@broadcom.com>
2018-08-30 16:53:49 -0700

    cypress: produce both dev|prod bootloaders and sage binaries.
    refs #SWANDROID-XXXX
    
    Change-Id: I7e3443ca03abfa6f3bedd43a7e63b406194f1d29
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- cypress.mk
+++ cypress.mk
@@ -45,6 +45,11 @@ endif
 export LOCAL_DEVICE_SEPOLICY_BLOCK
 export LOCAL_DEVICE_USE_VERITY   := y
 
+# bootloader firmware manipulation.
+export LOCAL_DEVICE_SAGE_DEV_N_PROD := y
+export BOLT_IMG_SWAP_BBL            := device/broadcom/cypress/blb/zb/bbl-3.1.1-zb.bin
+export BOLT_IMG_SWAP_BFW            := device/broadcom/cypress/blb/zb/bfw-4.2.5-zb.bin
+
 # common to all cypress devices.
 include device/broadcom/cypress/common.mk
 
@@ -61,6 +66,11 @@ endif
 export HW_HVD_REVISION           := S
 # v3d mmu available.
 export HW_GPU_MMU_SUPPORT        := y
+
+# bootloader firmware manipulation.
+export LOCAL_DEVICE_SAGE_DEV_N_PROD := y
+export BOLT_IMG_SWAP_BBL            := device/broadcom/cypress/blb/zb/bbl-3.1.1-zb.bin
+export BOLT_IMG_SWAP_BFW            := device/broadcom/cypress/blb/zb/bfw-4.2.5-zb.bin
 
 # baseline the common support.
 $(call inherit-product, device/broadcom/common/bcm.mk)
