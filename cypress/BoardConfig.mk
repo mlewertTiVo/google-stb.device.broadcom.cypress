8ee7461cdce9d425683b72b77d2886c44ddc877f
Pierre Couillaud <pierre.couillaud@broadcom.com>
2018-09-25 08:55:36 -0700

    cypress: fix up "nvi" platform against P|9.0
    refs #SWANDROID-5486
    
    Change-Id: I154b44997494c5e2d77be152e48782269fd13177
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- cypress/BoardConfig.mk
+++ cypress/BoardConfig.mk
@@ -1,2 +1,7 @@
+# do not generate vndk for nvi devices.
+ifeq ($(LOCAL_NVI_LAYOUT),y)
+BOARD_VNDK_RUNTIME_DISABLE := true
+endif
+
 include device/broadcom/common/BoardConfig.mk
 
--- cypress_nvi.mk
+++ cypress_nvi.mk
@@ -3,6 +3,11 @@ export LOCAL_NVI_LAYOUT := y
 export HW_DTU_SUPPORT   := n
 include device/broadcom/cypress/cypress.mk
 
+# falback to legacy linker namespace
+PRODUCT_TREBLE_LINKER_NAMESPACES_OVERRIDE := false
+# do not generate a vndk
+PRODUCT_USE_VNDK_OVERRIDE := false
+
 PRODUCT_NAME                   := cypress_nvi
 PRODUCT_MODEL                  := cypress
 PRODUCT_BRAND                  := broadcom
